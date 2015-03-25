2c1f764c4e651a3c5b8a72a90696798c326b32da
pandaoknight@iZ25xor03qdZ <pandaoknight@iZ25xor03qdZ.com>
2015-03-25 09:55:51 +0800

    测试namespace和use 结论：use的时候必须使用绝对命名空间。但代码中可以使用相对命名空间。

--- bin/main.php
+++ bin/main.php
@@ -3,8 +3,11 @@ namespace pandaoknight\hello_composer_package;
 
 require_once(__DIR__ . "/hello/Hello.class.php");
 
+use pandaoknight\hello_composer_package\hello\Hello as Hello;
 
-$hello_obj = new hello\Hello("Panda");
+
+$hello_obj = new Hello("Panda");
+#$hello_obj = new hello\Hello("Panda");
 $hello_obj->hello();
 
 
