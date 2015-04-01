3c5ae113c1002a6b40c8a7ec4e34fd7b83eaccd8
pandaoknight@iZ25xor03qdZ <pandaoknight@iZ25xor03qdZ.com>
2015-04-01 11:18:30 +0800

    +把 Hello.class.php 重命名为 Hello.php 以符合psr-4规范

--- bin/main.php
+++ bin/main.php
@@ -9,12 +9,14 @@ if(@include_once(__DIR__ . $project_mode_path)){
 }elseif(@include_once(__DIR__ . $vendor_mode_path)){
     #print "[OK] Vendor模式...\n";
 }else{
-    print "[Fail] $project_mode_path, $vendor_mode_path 均未找到composer的autoload.php。\n";
+    print "[Fail] $project_mode_path, $vendor_mode_path 均未找到composer的autoload.php。\n" .
+          "       请确认已经执行过 composer install / update 以保证依赖的包有正常安装。\n";
     die(-1);
 }
 #
 
-require_once(__DIR__ . "/hello/Hello.class.php");
+require_once(__DIR__ . "/hello/Hello.php");
+#require_once(__DIR__ . "/hello/Hello.class.php");
 
 use pandaoknight\hello_composer_package\hello\Hello;
 #use pandaoknight\hello_composer_package\hello\Hello as Hello; // 没有必要使用别名
